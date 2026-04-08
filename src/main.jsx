8ac1ed6f1d0eb2ff5d1d3c66acf3e6757f5c9c23
andrewjconstan <47575631+andrewjconstan@users.noreply.github.com>
2026-04-08 15:50:31 -0700

    404.html page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+const redirect = new URLSearchParams(window.location.search).get('redirect')
+if (redirect) {
+  window.history.replaceState(null, '', redirect)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/oddlot-coffee">
